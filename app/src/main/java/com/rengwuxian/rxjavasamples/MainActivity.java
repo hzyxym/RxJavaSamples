1c97b948984416eb3a3bb16701f64a7e7e00eb3b
黄镇营 <953624084@qq.com>
2017-03-13 23:17:30 +0800

    Update MainActivity.java

--- app/src/main/java/com/rengwuxian/rxjavasamples/MainActivity.java
+++ app/src/main/java/com/rengwuxian/rxjavasamples/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setSupportActionBar(toolBar);
-        //this edit by app.
+        //this edit by web.
 
         viewPager.setAdapter(new FragmentPagerAdapter(getFragmentManager()) {
             @Override
