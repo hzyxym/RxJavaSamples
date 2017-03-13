598d14f1eeb6d63e101ee564298cacbb7de7f6d6
hzyxym <953624084@qq.com>
2017-03-14 00:31:49 +0800

    test

--- app/src/main/java/com/rengwuxian/rxjavasamples/MainActivity.java
+++ app/src/main/java/com/rengwuxian/rxjavasamples/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setSupportActionBar(toolBar);
+        // test
 
         viewPager.setAdapter(new FragmentPagerAdapter(getFragmentManager()) {
             @Override
